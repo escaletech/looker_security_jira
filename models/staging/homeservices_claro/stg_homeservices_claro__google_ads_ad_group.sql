8d9513b1e7e31538513591b97238f09b4b33f897
FELIPE MAGALHAES <felipe.magalhaes@escale.com.br>
2024-10-09 19:17:02 +0000

    a

--- models/staging/homeservices_claro/stg_homeservices_claro__google_ads_ad_group.sql
+++ models/staging/homeservices_claro/stg_homeservices_claro__google_ads_ad_group.sql
@@ -1,7 +1,7 @@
 with source as (
     select
         *
-    from {{ source('source_name', 'object_name') }}
+    from {{ source('homeservices_claro', 'google_ads_ad_group') }}
 )
 , build_source as (
     SELECT
