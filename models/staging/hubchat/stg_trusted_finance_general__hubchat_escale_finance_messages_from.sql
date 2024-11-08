fcd1af0bbdc01e9a136f1b56041d692e1f4619cf
FELIPE MAGALHAES <felipe.magalhaes@escale.com.br>
2024-11-08 13:33:43 +0000

    .

---
+++ models/staging/hubchat/stg_trusted_finance_general__hubchat_escale_finance_messages_from.sql
@@ -0,0 +1,30 @@
+with 
+
+source as (
+
+    select * from {{ source('trusted_finance_general', 'hubchat_escale_finance_messages_from') }}
+
+),
+
+renamed as (
+
+    select
+        __v,
+        _id,
+        lake_created_at,
+        origin,
+        session_init,
+        timestamp,
+        timestamp_init,
+        token,
+        user_agent,
+        year,
+        month,
+        day,
+        lake_key
+
+    from source
+
+)
+
+select * from renamed
